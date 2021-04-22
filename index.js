a267f2fc5aea081af988683de2b68697685457e3
Blake Julander <13lakejulander@gmail.com>
2021-04-22 13:25:51 -0700

    commit tree bb24b49df13a2fec9e67a8c8df9021477ea802f9

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution here
+
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'bobby']
+
+function findMatching(array, name){
+    return array.filter(i => i.toLowerCase() === name.toLowerCase())
+}
+
+// function fuzzyMatch(drivers, letter){
+//     return drivers.filter(name => name.charAt(0) === letter)
+// }
+
+function fuzzyMatch(source, testString) {
+    return source.filter( possibleMatch => possibleMatch.toLowerCase().indexOf(testString.toLowerCase()) === 0)
+}
+
+function matchName(drivers, matchName){
+    return drivers.filter(i => i.name === matchName)
+}
+
+
